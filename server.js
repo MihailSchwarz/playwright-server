933cb36c17cf79a0c84d632d62db220f7645a256
MihailSchwarz <mihawin@ya.ru>
2023-04-18 14:25:45 +0100

    upd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { firefox } = require("playwright");
 
 const app = express();
-const PORT = 3000;
+const PORT = 30823;
 
 let browser;
 let context;
@@ -13,20 +13,26 @@ let context;
   context = await browser.newContext();
 })();
 
-app.get("/", async (req, res) => {
+app.get("/r2/", async (req, res) => {
   const url = req.query.url;
 
   const page = await context.newPage();
   const url2 =
     "https://services.rome2rio.com/api/1.5/json/search?key=jGq3Luw3&oName=%D0%9F%D1%80%D0%B0%D0%B3%D0%B0%2C+%D0%98%D1%81%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F&dName=%D0%90%D0%BB%D0%B8%D0%BA%D0%B0%D0%BD%D1%82%D0%B5%2C+%D0%98%D1%81%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F&languageCode=ru&currencyCode=EUR&uid=ISRey20210619084420183ufdd&aqid=ISRey20210619084420183ufdd&analytics=false&debugFeatures=false&groupOperators=false";
-  await page.goto(url2);
 
-  const html = await page.content();
-  //console.log(html);
+  if (url) {
+    await page.goto(url);
 
-  await page.close();
+    const html = await page.content();
 
-  res.send(html);
+    await page.close();
+
+    res.send(html);
+  } else res.send("ok");
+});
+
+app.get("/", async (req, res) => {
+  res.send("👋");
 });
 
 app.listen(PORT, () => {
