68dba7c2877dadb03241114174e72023434098de
MihailSchwarz <mihawin@ya.ru>
2023-04-18 22:34:32 +0100

    upd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       script: "server.js",
       instances: "max",
       exec_mode: "cluster",
+      watch: true,
+      ignore_watch: ["node_modules"],
       env: {
         NODE_ENV: "production",
       },
