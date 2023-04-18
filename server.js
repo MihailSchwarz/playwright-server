447e8b7efd3962b325dde3bffa8c082adfe5da57
MihailSchwarz <mihawin@ya.ru>
2023-04-18 14:11:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/", async (req, res) => {
   const page = await context.newPage();
   const url2 =
     "https://services.rome2rio.com/api/1.5/json/search?key=jGq3Luw3&oName=%D0%9F%D1%80%D0%B0%D0%B3%D0%B0%2C+%D0%98%D1%81%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F&dName=%D0%90%D0%BB%D0%B8%D0%BA%D0%B0%D0%BD%D1%82%D0%B5%2C+%D0%98%D1%81%D0%BF%D0%B0%D0%BD%D0%B8%D1%8F&languageCode=ru&currencyCode=EUR&uid=ISRey20210619084420183ufdd&aqid=ISRey20210619084420183ufdd&analytics=false&debugFeatures=false&groupOperators=false";
-  await page.goto(url);
+  await page.goto(url2);
 
   const html = await page.content();
   //console.log(html);
